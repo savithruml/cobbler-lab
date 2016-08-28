d55eb47af726a496209608bcf817dcd4c543582d
Savvy_Through <savithruml@users.noreply.github.com>
2016-08-28 00:44:21 -0700

    Create wgets.sh

---
+++ wgets.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/jameswthorne/default-ubuntu-sources.list/master/trusty-sources.list -O /var/www/html/trusty-sources.list
+
+wget https://raw.githubusercontent.com/jameswthorne/preseeds/master/ubuntu-server-14.04-unattended-cobbler.seed -O /var/lib/cobbler/kickstarts/ubuntu-server-14.04-unattended-cobbler.seed
+
